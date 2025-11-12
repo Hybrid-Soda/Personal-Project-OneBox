cf4094fadd778e97633e686aaae0f507939a0725
임경태 <limkt0925@gmail.com>
2025-11-12 17:25:36 +0900

    Feat: application.yml

---
+++ src/main/java/com/devnovus/oneBox/domain/MetadataRepository.java
@@ -0,0 +1,6 @@
+package com.devnovus.oneBox.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MetadataRepository extends JpaRepository<Metadata, Long> {
+}
