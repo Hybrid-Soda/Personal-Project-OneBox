89ffdacb9a0ba6760357abedaadb2b78dc288d97
임경태 <limkt0925@gmail.com>
2025-11-13 02:37:11 +0900

    Feat: loggingFilter 구현

--- src/main/java/com/devnovus/oneBox/web/folder/dto/CreateFolderRequest.java
+++ src/main/java/com/devnovus/oneBox/web/folder/dto/CreateFolderRequest.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateFolderRequest {
-    private Long UserId;         // 유저아이디
+    private Long userId;         // 유저아이디
     private Long parentFolderId; // 상위폴더아이디
     private String folderName;   // 폴더이름
 }
